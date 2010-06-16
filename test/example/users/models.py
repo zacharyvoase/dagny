73f7089db8dce2e9957e9f553e9f38d0c99a1ae1
Zachary Voase <z@zacharyvoase.com>
2010-06-16 22:37:28 +0000

    Empty files deserve poetry.

--- test/example/users/models.py
+++ test/example/users/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+## Empty files deserve poetry.
 
-# Create your models here.
+# She dwelt among the untrodden ways
+# Beside the springs of Dove,
+# A Maid whom there were none to praise
+# And very few to love:
+#  
+# A violet by a mossy stone
+# Half hidden from the eye!
+# Fair as a star, when only one
+# Is shining in the sky.
+#  
+# She lived unknown, and few could know
+# When Lucy ceased to be;
+# But she is in her grave, and oh,
+# The difference to me!
+# 
+# -- William Wordsworth
