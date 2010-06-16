916b353a74b312a9370a3d7eb962a50c837e6895
Zachary Voase <z@zacharyvoase.com>
2010-06-16 21:20:11 +0000

    Added a small example project, for testing purposes.

---
+++ test/example/settings.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# Django settings for example project.
+
+import os
+import sys
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Zachary Voase', 'z@zacharyvoase'),
+)
+
+MANAGERS = ADMINS
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(PROJECT_ROOT)
+
+DB_DIR = os.path.join(PROJECT_ROOT, 'db')
+os.makedirs(DB_DIR)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DB_DIR, 'development.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'GMT'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+
+USE_I18N = False
+USE_L10N = True
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+
+SECRET_KEY = 'c-!9fgws_aa5fyybk97da5xz63dxhuuxczlal76k!5d#i7vuo&'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'example.urls'
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, 'templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    
+    'users',
+)
