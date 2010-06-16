dadd75f9915bea02435bf3938cd9d8e9c63c7910
Zachary Voase <z@zacharyvoase.com>
2010-06-16 23:34:34 +0000

    Hooked up /users/ in the URLconf.

--- test/example/urls.py
+++ test/example/urls.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from dagny.urls import resources
 from django.conf.urls.defaults import *
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    
+    (r'^users/', resources('users.resources.User', name='User')),
     
     (r'^admin/', include(admin.site.urls)),
 )
