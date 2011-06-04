40b03cd63ac0e43305cb4295b2fee5aab943eac4
Zachary Voase <z@zacharyvoase.com>
2011-06-04 20:27:39 +0000

    Modified routing tests to expect method -> action maps instead of simple action names in resolved URLs.

--- example/users/tests/test_routing.py
+++ example/users/tests/test_routing.py
@@ -4,6 +4,20 @@ from django.test import TestCase
 from users import resources
 
 
+COLLECTION_METHODS = {
+    'GET': 'index',
+    'POST': 'create'
+}
+MEMBER_METHODS = {
+    'GET': 'show',
+    'POST': 'update',
+    'PUT': 'update',
+    'DELETE': 'destroy'
+}
+EDIT_METHODS = {'GET': 'edit'}
+NEW_METHODS = {'GET': 'new'}
+
+
 class DefaultRoutingTest(TestCase):
 
     def assert_resolves(self, url, func, *args, **kwargs):
@@ -19,11 +33,13 @@ class DefaultRoutingTest(TestCase):
 
     def test_index(self):
         self.assertEqual(reverse('User#index'), '/users/')
-        self.assert_resolves('/users/', resources.User, action='index')
+        self.assert_resolves('/users/', resources.User,
+                             methods=COLLECTION_METHODS)
 
     def test_show(self):
         self.assertEqual(reverse('User#show', args=(1,)), '/users/1/')
-        self.assert_resolves('/users/1/', resources.User, '1', action='show')
+        self.assert_resolves('/users/1/', resources.User,
+                             '1', methods=MEMBER_METHODS)
 
         # Fails for invalid IDs.
         self.assertRaises(NoReverseMatch, reverse, 'User#show',
@@ -31,22 +47,27 @@ class DefaultRoutingTest(TestCase):
 
     def test_new(self):
         self.assertEqual(reverse('User#new'), '/users/new/')
-        self.assert_resolves('/users/new/', resources.User, action='new')
+        self.assert_resolves('/users/new/', resources.User,
+                             methods=NEW_METHODS)
 
     def test_edit(self):
         self.assertEqual(reverse('User#edit', args=(1,)), '/users/1/edit/')
-        self.assert_resolves('/users/1/edit/', resources.User, '1', action='edit')
+        self.assert_resolves('/users/1/edit/', resources.User,
+                             '1', methods=EDIT_METHODS)
 
 
 class AtomPubRoutingTest(DefaultRoutingTest):
 
     def test_index(self):
         self.assertEqual(reverse('UserAtomPub#index'), '/users-atompub/')
-        self.assert_resolves('/users-atompub/', resources.UserAtomPub, action='index')
+        self.assert_resolves('/users-atompub/', resources.UserAtomPub,
+                             methods=COLLECTION_METHODS)
 
     def test_show(self):
-        self.assertEqual(reverse('UserAtomPub#show', args=(1,)), '/users-atompub/1')
-        self.assert_resolves('/users-atompub/1', resources.UserAtomPub, '1', action='show')
+        self.assertEqual(reverse('UserAtomPub#show', args=(1,)),
+                         '/users-atompub/1')
+        self.assert_resolves('/users-atompub/1', resources.UserAtomPub,
+                             '1', methods=MEMBER_METHODS)
 
         # Fails for invalid IDs.
         self.assertRaises(NoReverseMatch, reverse, 'UserAtomPub#show',
@@ -54,32 +75,40 @@ class AtomPubRoutingTest(DefaultRoutingTest):
 
     def test_new(self):
         self.assertEqual(reverse('UserAtomPub#new'), '/users-atompub/new')
-        self.assert_resolves('/users-atompub/new', resources.UserAtomPub, action='new')
+        self.assert_resolves('/users-atompub/new', resources.UserAtomPub,
+                             methods=NEW_METHODS)
 
     def test_edit(self):
-        self.assertEqual(reverse('UserAtomPub#edit', args=(1,)), '/users-atompub/1/edit')
-        self.assert_resolves('/users-atompub/1/edit', resources.UserAtomPub, '1', action='edit')
+        self.assertEqual(reverse('UserAtomPub#edit', args=(1,)),
+                         '/users-atompub/1/edit')
+        self.assert_resolves('/users-atompub/1/edit', resources.UserAtomPub,
+                             '1', methods=EDIT_METHODS)
 
 
 class RailsRoutingTest(DefaultRoutingTest):
 
     def test_index(self):
         self.assertEqual(reverse('UserRails#index'), '/users-rails')
-        self.assert_resolves('/users-rails', resources.UserRails, action='index')
-        self.assert_resolves('/users-rails/', resources.UserRails, action='index')
+        self.assert_resolves('/users-rails', resources.UserRails,
+                             methods=COLLECTION_METHODS)
+        self.assert_resolves('/users-rails/', resources.UserRails,
+                             methods=COLLECTION_METHODS)
 
     def test_index_with_format(self):
-        self.assertEqual(reverse('UserRails#index', kwargs={'format': '.json'}), '/users-rails.json')
-        self.assert_resolves('/users-rails.json', resources.UserRails, action='index', format='.json')
+        self.assertEqual(reverse('UserRails#index', kwargs={'format': '.json'}),
+                         '/users-rails.json')
+        self.assert_resolves('/users-rails.json', resources.UserRails,
+                             methods=COLLECTION_METHODS, format='.json')
 
     def test_show(self):
-        self.assertEqual(reverse('UserRails#show', kwargs={'id': 1}), '/users-rails/1')
+        self.assertEqual(reverse('UserRails#show', kwargs={'id': 1}),
+                         '/users-rails/1')
         self.assert_resolves('/users-rails/1',
                              resources.UserRails,
-                             id='1', action='show')
+                             id='1', methods=MEMBER_METHODS)
         self.assert_resolves('/users-rails/1/',
                              resources.UserRails,
-                             id='1', action='show')
+                             id='1', methods=MEMBER_METHODS)
 
         # Fails for invalid IDs.
         self.assertRaises(NoReverseMatch, reverse, 'UserRails#show',
@@ -94,18 +123,23 @@ class RailsRoutingTest(DefaultRoutingTest):
                          '/users-rails/1.json')
         self.assert_resolves('/users-rails/1.json',
                              resources.UserRails,
-                             id='1', action='show', format='.json')
+                             id='1', methods=MEMBER_METHODS, format='.json')
 
     def test_new(self):
         self.assertEqual(reverse('UserRails#new'), '/users-rails/new')
-        self.assert_resolves('/users-rails/new', resources.UserRails, action='new')
-        self.assert_resolves('/users-rails/new/', resources.UserRails, action='new')
+        self.assert_resolves('/users-rails/new', resources.UserRails,
+                             methods=NEW_METHODS)
+        self.assert_resolves('/users-rails/new/', resources.UserRails,
+                             methods=NEW_METHODS)
         self.assertRaises(Resolver404, resolve, '/users-rails/new/foobar')
         self.assertRaises(Resolver404, resolve, '/users-rails/new.foobar')
 
     def test_edit(self):
-        self.assertEqual(reverse('UserRails#edit', kwargs={'id': 1}), '/users-rails/1/edit')
-        self.assert_resolves('/users-rails/1/edit', resources.UserRails, id='1', action='edit')
-        self.assert_resolves('/users-rails/1/edit/', resources.UserRails, id='1', action='edit')
+        self.assertEqual(reverse('UserRails#edit', kwargs={'id': 1}),
+                         '/users-rails/1/edit')
+        self.assert_resolves('/users-rails/1/edit', resources.UserRails,
+                             id='1', methods=EDIT_METHODS)
+        self.assert_resolves('/users-rails/1/edit/', resources.UserRails,
+                             id='1', methods=EDIT_METHODS)
         self.assertRaises(Resolver404, resolve, '/users-rails/1/edit/foobar')
         self.assertRaises(Resolver404, resolve, '/users-rails/1/edit.foobar')
